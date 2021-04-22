10e0066f74370b0f3a2ec059e90e74f522894884
Thomas Boilesen <thomasboilesen@gmail.com>
2021-04-22 15:17:22 -0300

    quase  css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Header from './components/Header.jsx';
+import Header from './components/Header';
 import Beers from './components/Beers.jsx';
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer.jsx';
 import Detail from './components/Detail';
 import './App.css';
+import Card from './components/Card.jsx';
 
 const App = (props) => {
   return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/beers" component={Beers} />
-        <Route exact path="/beers/:id" component={Detail} />
-        <Route exact path="/random-beer" component={RandomBeer} />
-        <Route exact path="/new-beer" component={NewBeer} />
-      </Switch>
-    </div>
+    <Card titulo="BREJA" color="#f28e1c">
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/beers" component={Beers} />
+          <Route exact path="/beers/:id" component={Detail} />
+          <Route exact path="/random-beer" component={RandomBeer} />
+          <Route exact path="/new-beer" component={NewBeer} />
+        </Switch>
+      </div>
+    </Card>
   );
 };
 
